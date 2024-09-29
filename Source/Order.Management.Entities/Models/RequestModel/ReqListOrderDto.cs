04b1d2f76fb7e56114be9cdb5c812580e9e26c59
QuocViet <75932337+QuocViet22@users.noreply.github.com>
2024-09-29 23:25:13 +0700

    10. Add filter conditional when get list order (#17)

--- Source/Order.Management.Entities/Models/RequestModel/ReqListOrderDto.cs
+++ Source/Order.Management.Entities/Models/RequestModel/ReqListOrderDto.cs
@@ -8,5 +8,11 @@
         public int PageIndex { get; set; }
 
         public int PageSize { get; set; }
+
+        public string? DateFrom { get; set; }
+
+        public string? DateTo { get; set; }
+
+        public IEnumerable<string>? Status { get; set; }
     }
 }
